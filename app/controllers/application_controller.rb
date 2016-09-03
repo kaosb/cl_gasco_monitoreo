d4fe138925fba641640e643acbe97011951fabc8
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-09-03 01:42:00 -0300

    Avance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
+
+	# Ejemplo call servicio.
+	def dashboard
+		require 'savon'
+		client = Savon.client(wsdl: 'http://smtp.gasco.cl/WSGasco/WSPedidosOnline/WSGranelSimula.asmx?wsdl')
+		response = client.call(:get_sector_direccion, message: { codigo_cliente: 12287887, tipo_pedido: "G", v_korg: "7200" })
+		render json: { status: true, available_operations: client.operations, response: response.body }, status: 200
+	end
+
 end
