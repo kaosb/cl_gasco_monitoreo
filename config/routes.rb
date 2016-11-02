921298168283972407960a6fce7aeb8615d8278b
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-11-02 04:45:49 -0300

    Avances

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-	# root to: "reports#dashboard"
-	root to: "reports#index"
-	get 'reports/dashboard'
+	devise_for :users
 
+	root to: "reports#index"
+	
 	scope '/services' do
 		get '/show/:service_id' => 'services#show'
 	end
 
+	# root to: "reports#dashboard"
+	# get 'reports/dashboard'
 	# get 'reports/rfcweb'
 	# get 'reports/index'
 	# get 'services/wsrfcweb_consultaservicio'
