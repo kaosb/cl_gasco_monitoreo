09889e3171b08254c973153273f738bdade43e46
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2017-04-21 17:05:49 -0300

    add endpoint to get latest activity

--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,6 @@ Rails.application.routes.draw do
 	post 'add_alert_receiver' => 'alert#add_alert_receiver'
 	post 'update_alert_receiver' => 'alert#update_alert_receiver'
 	post 'delete_alert_receiver' => 'alert#delete_alert_receiver'
+	get  'get_recent_activity' => 'services#recent_activity'
 
 end
