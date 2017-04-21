05329fb41f29a9a5753fb1c4dcc39d6f13aeaa57
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2017-04-21 17:07:52 -0300

    Fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 	
 	protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
 	# protect_from_forgery with: :exception
-	before_action :authenticate_user!
+	before_action :authenticate_user!, :except => [:recent_activity]
 
 	layout :layout_by_resource
 
